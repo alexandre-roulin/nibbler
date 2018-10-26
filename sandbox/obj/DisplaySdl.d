579ebf8e1a32db048da28e47a2c413fdaa53cbfc
Jino42 <ntoniolo@student.42.fr>
2018-10-26 16:35:38 +0200

    commun files between allexternal lib

--- sandbox/obj/DisplaySdl.d
+++
@@ -1,2 +0,0 @@
-obj/DisplaySdl.o: src/DisplaySdl.cpp include/DisplaySdl.hpp \
-  include/IDisplay.hpp include/Vector2D.tpp include/Grid.tpp
--- sandbox/obj/main.d
+++
@@ -1,2 +0,0 @@
-obj/main.o: src/main.cpp include/DisplaySdl.hpp include/IDisplay.hpp \
-  include/Vector2D.tpp include/Grid.tpp
