7c295565e613c06badb8c90aa26bae943d1e7ddf
Jino <ntoniolo@student.42.fr>
2018-11-13 21:38:48 +0100

    Change name option

--- class/gui/widget/WidgetOption.cpp
+++ class/gui/widget/WidgetOption.cpp
@@ -1,3 +1,4 @@
+#include <gui/Core.hpp>
 #include "WidgetOption.hpp"
 #include "Univers.hpp"
 #include "imgui.h"
@@ -5,6 +6,7 @@
 WidgetOption::WidgetOption(Core &core) :
 AWidget(core)
 {
+	memcpy(this->_nameBuffer, this->_core.univers.getClientTCP_().getSnake().name, NAME_BUFFER);
 }
 
 WidgetOption::~WidgetOption(void)
@@ -15,6 +17,16 @@ void			WidgetOption::render(void)
 	ImGui::Begin("TEST", NULL, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoCollapse);
 //	ImGui::InputInt("Size of Map", &i0);
 //	ImGui::InputInt2("input int2", vec4i);
+	void change_name(char const *name);
+
+
+	if (ImGui::InputText("Name", this->_nameBuffer,
+						 IM_ARRAYSIZE(this->_nameBuffer),
+						 ImGuiInputTextFlags_EnterReturnsTrue)) {
+
+		this->_core.univers.getClientTCP_().change_name(this->_nameBuffer);
+		memcpy(this->_nameBuffer, this->_core.univers.getClientTCP_().getSnake().name, NAME_BUFFER);
+	}
 	ImGui::End();
 
 }
