1641fe4ee3f6e6d673c38f6401ae88d4339c1147
Krystalz42 <aroulin@student.42.fr>
2018-11-19 17:35:48 +0100

    Merge

--- class/component/CollisionComponent.cpp
+++ class/component/CollisionComponent.cpp
@@ -1,16 +1,7 @@
 #include <cassert>
 #include <iostream>
 #include "CollisionComponent.hpp"
-std::string const CollisionComponent::NAME_COMPONENT = "collisionComponent";
-std::string const CollisionComponent::IS_WALL = "isWall";
 
 CollisionComponent::CollisionComponent() {
 
-}
-
-Json::Value CollisionComponent::serializeComponent() {
-	return Json::Value();
-}
-
-CollisionComponent::CollisionComponent(Json::Value json) {
 }
